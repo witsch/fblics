5d6597983fc3ce77d8026386f3da94d7feed42f4
Andreas Zeidler <az@zitc.de>
2024-09-03 00:51:31 +0200

    allow to dump all JSON-LD parts embedded in the given page
      $ pipenv run fblics --json https://www.kicker.de/1-fc-union-berlin/spielplan

--- fblics.py
+++ fblics.py
@@ -2,13 +2,14 @@ from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 from html import unescape
 from ics import Calendar, Event
-from json import loads
+from json import loads, dumps
 from requests import get
 
 
 def parse_arguments():
     parser = ArgumentParser(description='convert kicker.de schedule to ICS')
     parser.add_argument('url', help='URL to fetch game schedules from')
+    parser.add_argument('-j', '--json', action='store_true', help='dump all JSON-LD parts')
     return parser.parse_args()
 
 
@@ -19,10 +20,13 @@ def main():
     soup = BeautifulSoup(resp.text, 'html.parser')
     for match in soup.find_all('script', type='application/ld+json'):
         data = loads(unescape(match.string))
-        if data['@type'] == 'SportsEvent':
+        if args.json:
+            print(dumps(data, indent=2))
+        elif data['@type'] == 'SportsEvent':
             cal.events.add(Event(
                 name=data['name'],
                 begin=data['startDate'],
                 duration=dict(hours=1, minutes=45),
                 url='https://www.kicker.de' + data['url']))
-    print(cal.serialize())
+    if not args.json:
+        print(cal.serialize())
